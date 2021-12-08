77fce0df65467e0dc37bb6a491a381ee8b486d51
tencentcloudapi <tencentcloudapi@tencent.com>
2021-12-08 09:24:28 +0800

    release 3.0.536

--- src/TencentCloud/Tcss/V20201101/Models/CreateCheckComponentResponse.php
+++ src/TencentCloud/Tcss/V20201101/Models/CreateCheckComponentResponse.php
@@ -20,15 +20,15 @@ use TencentCloud\Common\AbstractModel;
 /**
  * CreateCheckComponent返回参数结构体
  *
- * @method string getInstallResult() 获取“InstallSucc"表示安装成功，"InstallFailed"表示安装失败
- * @method void setInstallResult(string $InstallResult) 设置“InstallSucc"表示安装成功，"InstallFailed"表示安装失败
+ * @method string getInstallResult() 获取"InstallSucc"表示安装成功，"InstallFailed"表示安装失败
+ * @method void setInstallResult(string $InstallResult) 设置"InstallSucc"表示安装成功，"InstallFailed"表示安装失败
  * @method string getRequestId() 获取唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
  * @method void setRequestId(string $RequestId) 设置唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
  */
 class CreateCheckComponentResponse extends AbstractModel
 {
     /**
-     * @var string “InstallSucc"表示安装成功，"InstallFailed"表示安装失败
+     * @var string "InstallSucc"表示安装成功，"InstallFailed"表示安装失败
      */
     public $InstallResult;
 
@@ -38,7 +38,7 @@ class CreateCheckComponentResponse extends AbstractModel
     public $RequestId;
 
     /**
-     * @param string $InstallResult “InstallSucc"表示安装成功，"InstallFailed"表示安装失败
+     * @param string $InstallResult "InstallSucc"表示安装成功，"InstallFailed"表示安装失败
      * @param string $RequestId 唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
      */
     function __construct()
